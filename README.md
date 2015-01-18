1202884ebe304ca683be7e4ef6b67f9bfb38bded
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2015-01-17 19:15:22 -0500

    Fixed RSS, it works, added README

---
+++ README.md
@@ -0,0 +1,19 @@
+# MIT OpenCourseWare RSS feed generator
+
+Simple script to read [MIT OpenCourseWare](http://ocw.mit.edu/index.htm) XML files and generate a RSS feed that's compatible with podcast apps, like Pocket Casts.
+
+So far there's only one, if it works well I'll probably generate more, if you want one let me know through GitHub issues, it should be easy to create others if the XML file follows the same format.
+
+I'll publish the RSS feeds on http://geekout.fm/mit/, here's the list of the available ones:
+* [Game Design - Philip B. Tan, Richard Eberhardt - CMS.608 - Spring 2014](http://ocw.mit.edu/courses/comparative-media-studies-writing/cms-608-game-design-spring-2014/index.htm)
+
+## Contributing
+
+This is a quick hack, it isn't suposed to live long. To add more courses just copy `generate_rss_for_` function and change the proper details.
+
+## Disclaimer
+
+All credit goes to MIT OCW, I'm just generating RSS feeds, [visit their site to see details about the courses and donate](http://ocw.mit.edu/index.htm).
+
+## License
+MIT, see details in [LICENSE](LICENSE)
--- mit-rss.rb
+++ mit-rss.rb
@@ -1,9 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+require 'erb'
 
 class Feed
-  attr_accessor :rss_url, :title, :url, :language, :title, :subtitle
-                :author, :summary, :author, :email, :image_url, :category
+  attr_accessor :rss_url, :title, :url, :language, :title, :subtitle,
+                :author, :summary, :author, :email, :image_url, :category,
+                :original_xml_url, :template_file, :output_file
 end
 
 class Episode
@@ -20,7 +22,7 @@ class Episode
   end
 
   def length_in_secs=(param)
-    @length = param.to_f
+    @length_in_secs = param.to_f
   end
 
   def short_description
@@ -31,16 +33,25 @@ class Episode
     Time.at(self.mtime).xmlschema
   end
 
-  def duration
+  def mp3_duration
     minutes = (self.length_in_secs / 60).truncate
     seconds = ((self.length_in_secs / 60).modulo(1) * 60).truncate
     duration = "%.2d:%.2d" % [minutes, seconds]
   end
 end
 
-def convert_mit_xml_to_rss_feed(mit_xml_url, feed_info, template_file)
-  episode_data = parse_xml(mit_xml_url)
-  generate_rss_feed(feed_info, episode_data, template_file)
+class Renderer
+  attr_reader :erb, :feed, :episodes
+
+  def initialize(erb_template_file, feed_info, episode_info)
+    @erb = ERB.new(File.open(erb_template_file).read)
+    @feed = feed_info
+    @episodes = episode_info
+  end
+ 
+  def result
+    @erb.result binding
+  end
 end
 
 def parse_xml(mit_xml_url)
@@ -75,26 +86,34 @@ def parse_xml(mit_xml_url)
   result
 end
 
-def generate_rss_feed(feed_info, episode_data, template_file)
-
+def generate_rss_feed(feed_info, episode_data)
+  renderer = Renderer.new(feed_info.template_file, feed_info, episode_data)
+  File.open(feed_info.output_file, 'w+') do |file|
+    file.write renderer.result
+  end
+  feed_info.output_file
 end
 
-def run
+def convert_mit_xml_to_rss_feed(feed_info)
+  episode_data = parse_xml(feed_info.original_xml_url)
+  generate_rss_feed(feed_info, episode_data)
+end
+
+def generate_rss_for_MITCMS_608JS14
   feed_info = Feed.new
-  feed_info.rss_url = 'http://rafaelrosafu.info/MITCMS.608JS14_rss.xml'
-  feed_info.title = 'MIT OpenCourseware - Game Design - Philip B. Tan, Richard Eberhardt - '
-  feed_info.url
-  feed_info.language
-  feed_info.title
-  feed_info.subtitle
-  feed_info.author
-  feed_info.summary
-  feed_info.author
-  feed_info.email
-  feed_info.image_url
-  feed_info.category
+  feed_info.original_xml_url  = 'http://ia902606.us.archive.org/10/items/MITCMS.608JS14/MITCMS.608JS14_files.xml'
+  feed_info.template_file     = './feed.xml.erb'
+  feed_info.output_file       = 'MITCMS_608JS14_rss.xml'
+  feed_info.rss_url           = "http://geekout.fm/mit/#{feed_info.output_file}"
+  feed_info.title             = 'MIT OpenCourseWare - Game Design - Philip B. Tan, Richard Eberhardt - CMS.608 - Spring 2014'
+  feed_info.url               = 'http://ocw.mit.edu/courses/comparative-media-studies-writing/cms-608-game-design-spring-2014/index.htm'
+  feed_info.language          = 'en-US'
+  feed_info.subtitle          = 'MIT OpenCourseWare - Game Design - CMS.608 - Spring 2014'
+  feed_info.author            = 'Philip B. Tan, Richard Eberhardt, students'
+  feed_info.summary           = "This course is built around practical instruction in the design and analysis of non-­digital games. It provides students the texts, tools, references, and historical context to analyze and compare game designs across a variety of genres. In teams, students design, develop, and thoroughly test their original games to better understand the interaction and evolution of game rules. Covers various genres and types of games, including sports, game shows, games of chance, card games, schoolyard games, board games, and role-­playing games."[0..249]
+  feed_info.email             = ''
+  feed_info.image_url         = 'http://ocw.mit.edu/courses/comparative-media-studies-writing/cms-608-game-design-spring-2014/cms-608s14.jpg'
+  feed_info.category          = 'Games &amp; Hobbies'
 
-
-  convert_mit_xml_to_rss_feed 'http://ia902606.us.archive.org/10/items/MITCMS.608JS14/MITCMS.608JS14_files.xml',
-                              {}, './feed.xml.erb'
+  convert_mit_xml_to_rss_feed feed_info
 end
